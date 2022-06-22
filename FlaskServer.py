de2feabdf167ff410a9a40f4941c237760a635e8
hamza shahid <hmshahid@iastate.edu>
2022-06-21 21:30:58 -0500

    errorHandler

--- FlaskServer.py
+++ FlaskServer.py
@@ -236,6 +236,15 @@ def logout():
 
         return render_template('login.html')
 
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == '__main__':
     app.secret_key = 'superSecretKey'
     app.config['SESSION_TYPE'] = 'filesystem'
